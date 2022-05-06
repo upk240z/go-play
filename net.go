4a40e4f91c1e1aad755dde30e3018e6e4c172682
Y.Hasegawa <hasegawa@zaf.jp>
2022-05-06 16:29:28 +0900

    http sender json body

--- net.go
+++ net.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
@@ -11,16 +12,12 @@ import (
 	"strings"
 )
 
-func main() {
+func getJson(url string) {
 	var err error
 	var res *http.Response
 	var body []byte
 
-	if _, envErr := os.Stat(".env"); envErr == nil {
-		godotenv.Load(".env")
-	}
-
-	if res, err = http.Get(os.Getenv("JSON_URL")); err != nil {
+	if res, err = http.Get(url); err != nil {
 		log.Fatal(err)
 	}
 
@@ -34,7 +31,7 @@ func main() {
 
 	for {
 		if err = decoder.Decode(&decoded); err == io.EOF {
-			fmt.Println("EOF")
+			fmt.Println("@@@@@ JSON fin @@@@")
 			break
 		} else if err != nil {
 			log.Fatal(err)
@@ -42,4 +39,48 @@ func main() {
 
 		fmt.Println(decoded)
 	}
+}
+
+func sendJson(url string, params interface{}) {
+	jsonData, err := json.Marshal(params)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bytes.NewReader(jsonData)
+	client := &http.Client{}
+	req, err := http.NewRequest(
+		"POST",
+		url,
+		reader,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+}
+
+func main() {
+	if _, envErr := os.Stat(".env"); envErr == nil {
+		godotenv.Load(".env")
+	}
+
+	getJson(os.Getenv("JSON_URL"))
+	sendJson(os.Getenv("ECHO_URL"), map[string]interface{}{
+		"string": "hello",
+		"number": 123,
+	})
 }
