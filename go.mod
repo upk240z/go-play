3ed756053bddcaed815392c449dd9a8badc8187b
Y.Hasegawa <hasegawa@zaf.jp>
2024-05-18 19:28:30 +0900

    packages updated

--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module play-ground
 go 1.20
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0
-	github.com/joho/godotenv v1.4.0
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
 )
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	gopkg.in/yaml.v2 v2.4.0
+	gorm.io/driver/sqlite v1.5.0
+	gorm.io/gorm v1.25.1
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- yaml.go
+++ yaml.go
@@ -3,14 +3,46 @@ package main
 import (
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"log"
 	"os"
 )
 
+type SimpleData struct {
+	Code string `yaml:"code"`
+	Age  int    `yaml:"age"`
+}
+
 func main() {
-	file, _ := os.Open("data/sample.yaml")
+	filePath := "data/simple.yaml"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	decoder := yaml.NewDecoder(file)
-	var result map[string]interface{}
-	decoder.Decode(&result)
-	file.Close()
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	var result SimpleData
+	if err := decoder.Decode(&result); err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(result)
+
+	structure := map[string]any{}
+	yamlBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := yaml.Unmarshal(yamlBytes, structure); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(structure)
 }
